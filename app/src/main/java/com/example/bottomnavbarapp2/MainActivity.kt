362151cd65f74b38a7441e63fb59c53165cb3ac0
FrancyAluma <francyaluma83@gmail.com>
2024-10-10 05:58:35 -0700

    second commit

--- app/src/main/java/com/example/bottomnavbarapp2/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavbarapp2/MainActivity.kt
@@ -131,7 +131,9 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         }
-                    ) {}
+                    ) {
+                        val padding = it
+                    }
                 }
 
             }
